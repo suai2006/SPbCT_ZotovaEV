ed1b752f1d98faef296ebb201bd216df1ff1d900
Вячеслав Гаврилюк <vvgavrilyuk@hotmail.com>
2022-03-02 23:44:05 +0300

    LR11_12

--- LR11_12/Stack.cpp
+++ LR11_12/Stack.cpp
@@ -2,75 +2,92 @@
 #include <iomanip>
 #include <string>
 #include <vector>
-using namespace std;
-
+#include "UserClass.h"
 template <class SType> class Stack
  {
-
 private:
     vector <SType> Bufer;
-
-    bool isEmpty() {
-        if (Bufer.size() == 0) {
-            return false;
-        }
-        return true;
-    }
+    bool isEmpty()const { return Bufer.size();}
 public:
     Stack() {}
     ~Stack() {}
 
     void push(const SType& i) {
-
         Bufer.push_back(i);        
         cout << "Втолкнули в стэк: ";
         cout << i << endl;
     }
     
     void pop () {
-        if (isEmpty()) {
-            
-            cout << "Втолкнули из стэка: ";
+        if (isEmpty()) {            
+            cout << "Вытолкнули из стэка: ";
             cout << Bufer.back() << endl;
             Bufer.pop_back();
         }
-        else {
-            cout << "Стэк пуст" << endl;
-        }
-        
+        else cout << "Стэк пуст" << endl;
     }
-    
+    Stack<SType>& operator=(const Stack<SType>& ob)
+    {
+        Bufer = ob.Bufer;
+        return *this;
+    }
+    void operator + (const SType& i) {push(i);}
+    void operator -- () {pop();}
+    explicit operator bool() const {return isEmpty();}    
 };
+
+
 
 int main()
 {
+    using namespace std;
     setlocale(LC_ALL, "Russian");
-    Stack<int> stack;
+    Stack<int> StackInt;
 
-    for (int i = 0; i < 5; i++) {
-        stack.push(i);
-    }
+    for (int i = 0; i < 2; i++) StackInt + i;
 
     cout << endl;
 
-    for (int i = 0; i <= 5; i++) {
-        stack.pop();
-    }
+    for (int i = 0; i <= 2; i++)  --StackInt;
+
+    cout << endl;
+    cout << "Тестируем работу класса с вещественными числами" << endl;
+    cout << endl;
+
+    Stack<double> StackDouble;
+    StackDouble + 0.15;
+    StackDouble + 100.15;
+
+    cout << endl;
+
+    for (int i = 0; i <= 2; i++) --StackDouble;
 
     cout << endl;
     cout<< "Тестируем работу класса со строками" << endl;
     cout << endl;
 
-    Stack<string> stack1;
-    for (int i = 0; i < 5; i++) {
-        stack1.push("строка "+to_string(i));
-    }
+    Stack<string> StackStr;
+    for (int i = 0; i < 2; i++) StackStr + ("строка " + to_string(i));
+    cout << endl;
+    for (int i = 0; i <= 2; i++) --StackStr;
+
+    if(!StackStr) cout << "Тестируем работу перегрузки bool" << endl;
 
     cout << endl;
+    cout << "Тестируем работу класса с пользовательским классом как параметром" << endl;
+    cout << endl;
+    Stack<UserClass> UClass;
 
-    for (int i = 0; i <= 5; i++) {
-        stack1.pop();
-    }
+    UserClass u1;
+    UserClass u2;
+
+    UClass + u1;
+    UClass + u2;
+    cout << endl;
+    --UClass;
+    --UClass;
+    --UClass;
+
     return 0;
 }
 
